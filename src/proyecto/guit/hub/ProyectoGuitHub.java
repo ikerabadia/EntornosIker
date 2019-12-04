864057a39752ef6e81451009b05694be2482ec2f
Vespertino <Vespertino@PC013>
2019-12-04 21:31:57 +0100

    Escribe 10.000 jajahola

---
+++ src/proyecto/guit/hub/ProyectoGuitHub.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.guit.hub;
+
+/**
+ *
+ * @author Vespertino
+ */
+public class ProyectoGuitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i = 0; i < 10000; i++) {
+            System.out.println("jajahola");
+            
+        }
+    }
+    
+}
