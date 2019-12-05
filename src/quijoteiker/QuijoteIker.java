f147d8400a2a6945828207b8baa483244540b204
Vespertino <Vespertino@PC16>
2019-12-05 21:52:03 +0100

    en este perfil odiamos a israel

--- src/quijoteiker/QuijoteIker.java
+++ src/quijoteiker/QuijoteIker.java
@@ -26,7 +26,7 @@ public class QuijoteIker {
         apariciones = cuentaPalabras(palabra, fichero);
         System.out.println("El numero de veces que aparece la palabra introducida es de: " + apariciones);
     }
-    public static void hunterhuntergonmola(String[] args) {
+    public static void israelnoexiste(String[] args) {
         int apariciones = 0;
         String palabra;
         palabra = palabra();
